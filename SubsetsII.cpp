270e0e77ccc29e9a0e64a14ee289d03e37e4919a
veteran12 <zhangtong@172-26-35-31.dynapool.nyu.edu>
2014-06-01 18:07:14 -0400

    add python version

--- SubsetsII.cpp
+++ SubsetsII.cpp
@@ -26,3 +26,30 @@ public:
         return;
     }
 };
+
+/*
+ *python version
+ *class Solution:
+    # @param num, a list of integer
+    # @return a list of lists of integer
+    def subsetsWithDup(self, S):
+        S.sort()
+        res = []
+        for i in range( len(S) + 1 ):
+            tmp = []
+            self.sub( res, S, tmp, 0, i )
+        return res
+        
+    def sub(self, res, S, tmp, start, length):
+        if length == 0:
+            res.append(tmp[:]) //res.append(tmp) error this statement just insert a pointer which points to tmp into res,we need tmp[:] to make a copy!!! note: in  python sequence type is transfered as reference and regular type is transfered as value:)
+            return
+        while start < len(S):
+            tmp.append(S[start])
+            self.sub(res, S, tmp, start + 1, length - 1)
+            del tmp[-1]
+            while start + 1 < len(S) and S[start] == S[start + 1]:
+                start += 1
+            start += 1
+        return
+ */
