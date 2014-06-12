1038d70d8e8fc1aef784b4d86770212ee603e42a
veteran12 <zhangtong@172-29-1-62.dynapool.nyu.edu>
2014-06-11 21:08:57 -0400

    JumpGameII

---
+++ JumpGameII.cpp
@@ -0,0 +1,20 @@
+//greedy!!!
+
+class Solution {
+public:
+    int jump(int A[], int n) {
+        int start = 0;
+        int res = 0;
+        while (start < n-1)
+        {
+            res++;
+            if (start + A[start] >= n-1)
+                return res;
+            int mx = start;
+            for (int i = start + 1; i <= start + A[start]; ++i)
+                if (i + A[i] >= mx + A[mx])//find the farthest position can reach start from 'start' index and then begin from there.
+                    mx = i;
+            start = mx;
+        }
+    }
+};
